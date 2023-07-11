aa4c421ccfafc64319fd30499b285707cf1f86f4
Alex <alex97av@gmail.com>
2023-07-11 16:44:25 +0200

    Scrittura codice con printing

--- src/PrintYourNameAndSurname.java
+++ src/PrintYourNameAndSurname.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class PrintYourNameAndSurname {
+public class PrintYourNameAndSurname {
+    public static void main(String[] args){
+        String name="Alessandro";
+        String surname="Venuti";
+        System.out.print(name+" ");
+        System.out.print(surname );
+    }
 }
+//voleva due print giusto? altrimenti avrei scritto System.out.print(name+" "+surname);
